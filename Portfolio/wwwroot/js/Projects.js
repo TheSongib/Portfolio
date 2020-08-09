187db4b56502189c09a7aaf8af6dd021415b0746
Brandon Gibson <bcg600@gmail.com>
2020-08-09 03:12:26 -0400

    Check in of multiple additions

---
+++ Portfolio/wwwroot/js/Projects.js
@@ -0,0 +1,107 @@
+﻿function webGeneralButton() {
+    $("#webGeneralButton").addClass("active");
+    $("#webMoreInfoButton").removeClass("active");
+    $("#webVideoButton").removeClass("active");
+    $("#webGeneral").css("display", "block");
+    $("#webMoreinfo").css("display", "none");
+    $("#webVideo").css("display", "none");
+}
+
+function webVideoButton() {
+    $("#webGeneralButton").removeClass("active");
+    $("#webMoreInfoButton").removeClass("active");
+    $("#webVideoButton").addClass("active");
+    $("#webGeneral").css("display", "none");
+    $("#webMoreinfo").css("display", "none");
+    $("#webVideo").css("display", "block");
+}
+
+function webMoreInfoButton() {
+    $("#webGeneralButton").removeClass("active");
+    $("#webMoreInfoButton").addClass("active");
+    $("#webVideoButton").removeClass("active");
+    $("#webGeneral").css("display", "none");
+    $("#webVideo").css("display", "none");
+    $("#webMoreinfo").css("display", "block");
+}
+
+function agileGeneralButton() {
+    $("#agileGeneralButton").addClass("active");
+    $("#agileMoreInfoButton").removeClass("active");
+    $("#agileVideoButton").removeClass("active");
+    $("#agileGeneral").css("display", "block");
+    $("#agileMoreinfo").css("display", "none");
+    $("#agileVideo").css("display", "none");
+}
+
+function agileVideoButton() {
+    $("#agileGeneralButton").removeClass("active");
+    $("#agileMoreInfoButton").removeClass("active");
+    $("#agileVideoButton").addClass("active");
+    $("#agileGeneral").css("display", "none");
+    $("#agileMoreinfo").css("display", "none");
+    $("#agileVideo").css("display", "block");
+}
+
+function agileMoreInfoButton() {
+    $("#agileGeneralButton").removeClass("active");
+    $("#agileMoreInfoButton").addClass("active");
+    $("#agileVideoButton").removeClass("active");
+    $("#agileGeneral").css("display", "none");
+    $("#agileVideo").css("display", "none");
+    $("#agileMoreinfo").css("display", "block");
+}
+
+function portfolioGeneralButton() {
+    $("#portfolioGeneralButton").addClass("active");
+    $("#portfolioMoreInfoButton").removeClass("active");
+    $("#portfolioVideoButton").removeClass("active");
+    $("#portfolioGeneral").css("display", "block");
+    $("#portfolioMoreinfo").css("display", "none");
+    $("#portfolioVideo").css("display", "none");
+}
+
+function portfolioVideoButton() {
+    $("#portfolioGeneralButton").removeClass("active");
+    $("#portfolioMoreInfoButton").removeClass("active");
+    $("#portfolioVideoButton").addClass("active");
+    $("#portfolioGeneral").css("display", "none");
+    $("#portfolioMoreinfo").css("display", "none");
+    $("#portfolioVideo").css("display", "block");
+}
+
+function portfolioMoreInfoButton() {
+    $("#portfolioGeneralButton").removeClass("active");
+    $("#portfolioMoreInfoButton").addClass("active");
+    $("#portfolioVideoButton").removeClass("active");
+    $("#portfolioGeneral").css("display", "none");
+    $("#portfolioVideo").css("display", "none");
+    $("#portfolioMoreinfo").css("display", "block");
+}
+
+function gameGeneralButton() {
+    $("#gameGeneralButton").addClass("active");
+    $("#gameMoreInfoButton").removeClass("active");
+    $("#gameVideoButton").removeClass("active");
+    $("#gameGeneral").css("display", "block");
+    $("#gameMoreinfo").css("display", "none");
+    $("#gameVideo").css("display", "none");
+}
+
+function gameVideoButton() {
+    $("#gameGeneralButton").removeClass("active");
+    $("#gameMoreInfoButton").removeClass("active");
+    $("#gameVideoButton").addClass("active");
+    $("#gameGeneral").css("display", "none");
+    $("#gameMoreinfo").css("display", "none");
+    $("#gameVideo").css("display", "block");
+}
+
+function gameMoreInfoButton() {
+    $("#gameGeneralButton").removeClass("active");
+    $("#gameMoreInfoButton").addClass("active");
+    $("#gameVideoButton").removeClass("active");
+    $("#gameGeneral").css("display", "none");
+    $("#gameVideo").css("display", "none");
+    $("#gameMoreinfo").css("display", "block");
+}
